ad58d3250569939b4ad0564fbc2c5333518f4075
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2020-12-03 15:38:29 +0100

    Background Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import {CssBaseline} from '@material-ui/core';
 
+const useStyles = makeStyles(()=>({
+    background: {
+        minHeight: '100vh',
+        backgroundImage: `url(${process.env.PUBLIC_URL + "/Assets/bg.jpg"})`,
+    }
+}))
 const App = ()=>{
-    return <div></div>
+    const {background} = useStyles();
+
+    return <div className={background}>
+        <CssBaseline/>
+    </div>
 }
 
 export default App;
