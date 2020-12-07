bf3e30fe9d9754bb15ed63d1c275c328bc7ec103
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2020-12-07 16:27:43 +0100

    Header edited and new classes added to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,25 +4,60 @@ import { AppBar, IconButton, Toolbar } from "@material-ui/core";
 import SortIcon from "@material-ui/icons/Sort";
 
 const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh", 
+    fontFamily: "Nunito",
+  },
   appbar: {
-      background: "none",
+    background: "none",
+  },
+  icon: {
+    color: "#fff",
+    fontSize: "2rem",
+  },
+  appbarWrapper: {
+    width: "80%",
+    margin: "0 auto",
+  },
+  appbarTitle: {
+    flexGrow: 1,
+  },
+  colorText: {
+    color: "#5AFF",
+  },
+  container:{
+    textAlign: "center",
+  },
+  title : {
+      color: "#fff",
+      fontSize: "3rem",
 
   },
 }));
 
 const Header = () => {
-  const { appbar } = useStyles();
+  const classes = useStyles();
 
   return (
-    <div>
-      <AppBar className={appbar}>
-      <Toolbar>
-        <h1>MyIsland.</h1>
-        <IconButton>
-          <SortIcon />
-        </IconButton>
+    <div className={classes.root}>
+      <AppBar className={classes.appbar}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>
+            My<span className={classes.colorText}>Island.</span>
+          </h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <div className={classes.container}>
+        <h1 className={classes.title}>
+          Welcome to <br /> My <span className={classes.colorText}>Island.</span>
+        </h1>
+      </div>
     </div>
   );
 };
